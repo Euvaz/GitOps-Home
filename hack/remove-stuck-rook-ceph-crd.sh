9ee44830b228b6c81c896a74d50c6d674f3978f7
euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2022-09-04 14:04:11 -0700

    feat(hack): Add hack files

---
+++ hack/remove-stuck-rook-ceph-crd.sh
@@ -0,0 +1,4 @@
+for CRD in $(kubectl get crd -n rook-ceph | awk '/ceph.rook.io/ {print $1}'); do
+    kubectl get -n rook-ceph "$CRD" -o name | \
+    xargs -I {} kubectl patch -n rook-ceph {} --type merge -p '{"metadata":{"finalizers": [null]}}'
+done
